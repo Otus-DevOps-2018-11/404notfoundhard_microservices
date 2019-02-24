1b9a473feb0170d477d6b1d244525f4f5e371237
Stanislav Linnik <404notfoundhard@gmain.com>
2019-02-24 21:49:44 +0700

    move to default state

--- src/docker-run.sh
+++ src/docker-run.sh
@@ -1,5 +1,5 @@
 docker network create reddit
-docker run -d --network=reddit --network-alias=host_post_db --network-alias=host_db mongo:latest
-docker run -d --network=reddit --network-alias=host_post insecuritystone/post:1.0
-docker run -d --network=reddit --network-alias=host_comment insecuritystone/comment:1.0
+docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db mongo:latest
+docker run -d --network=reddit --network-alias=post insecuritystone/post:1.0
+docker run -d --network=reddit --network-alias=comment insecuritystone/comment:1.0
 docker run -d --network=reddit -p 9292:9292 insecuritystone/ui:1.0
